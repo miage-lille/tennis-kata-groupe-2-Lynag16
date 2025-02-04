bde58987967bb3959845fc2ac075589dc91e8530
Agouazi Lynda <lynda.agouazi.etu@b13p23.fil.univ-lille.fr>
2025-02-04 09:43:34 +0100

    complete the tp and add the necessary

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,8 +1,9 @@
 import { describe, expect, test } from '@jest/globals';
-import { otherPlayer, playerToString } from '..';
-// import * as fc from 'fast-check';
-
-// import * as G from './generators';
+import { otherPlayer, playerToString, scoreWhenAdvantage, scoreWhenDeuce, scoreWhenForty, scoreWhenPoint } from '..';
+import { deuce, advantage, game, forty, thirty, PointsData, Score } from '../types/score';
+import * as G from './generators';
+import * as fc from 'fast-check';
+import { isSamePlayer } from '../types/player';
 
 describe('Tests for tooling functions', () => {
   test('Given playerOne when playerToString', () => {
@@ -15,41 +16,89 @@ describe('Tests for tooling functions', () => {
 });
 
 describe('Tests for transition functions', () => {
-  // test('Given deuce, score is advantage to winner', () => {
-  //   console.log('To fill when we will know how represent Deuce');
-  // });
-  // test('Given advantage when advantagedPlayer wins, score is Game avantagedPlayer', () => {
-  //   console.log('To fill when we will know how represent Advantage');
-  // });
-  // test('Given advantage when otherPlayer wins, score is Deuce', () => {
-  //   console.log('To fill when we will know how represent Advantage');
-  // });
-  // test('Given a player at 40 when the same player wins, score is Game for this player', () => {
-  //   console.log('To fill when we will know how represent Forty');
-  // });
-  // test('Given player at 40 and other at 30 when other wins, score is Deuce', () => {
-  //   console.log('To fill when we will know how represent Forty');
-  // });
-  // test('Given player at 40 and other at 15 when other wins, score is 40 - 15', () => {
-  //   console.log('To fill when we will know how represent Forty');
-  // });
-  // -------------------------TESTS POINTS-------------------------- //
-  // test('Given players at 0 or 15 points score kind is still POINTS', () => {
-  // fc.assert(
-  //   fc.property(G.getPoints(), G.getPlayer(), ({ pointsData }, winner) => {
-  //     throw new Error(
-  //       'Your turn to code the preconditions, expected result and test.'
-  //     );
-  //   })
-  // );
-  // });
-  // test('Given one player at 30 and win, score kind is forty', () => {
-  // fc.assert(
-  //   fc.property(G.getPoints(), G.getPlayer(), ({ pointsData }, winner) => {
-  //     throw new Error(
-  //       'Your turn to code the preconditions, expected result and test.'
-  //     );
-  //   })
-  // );
-  // });
+  test('Given deuce, score is advantage to winner', () => {
+    fc.assert(
+      fc.property(G.getPlayer(), winner => {
+        const score = scoreWhenDeuce(winner);
+        const scoreExpected = advantage(winner);
+        expect(score).toStrictEqual(scoreExpected);
+      })
+    );
+  });
+
+  test('Given advantage when advantagedPlayer wins, score is Game advantagedPlayer', () => {
+    fc.assert(
+      fc.property(G.getPlayer(), G.getPlayer(), (advantagedPlayer, winner) => {
+        const score = scoreWhenAdvantage(advantagedPlayer, winner);
+        const scoreExpected = game(winner);
+        fc.pre(isSamePlayer(advantagedPlayer, winner));
+        expect(score).toStrictEqual(scoreExpected);
+      })
+    );
+  });
+
+  test('Given advantage when otherPlayer wins, score is Deuce', () => {
+    fc.assert(
+      fc.property(G.getPlayer(), G.getPlayer(), (advantagedPlayer, winner) => {
+        fc.pre(!isSamePlayer(advantagedPlayer, winner));
+        const score = scoreWhenAdvantage(advantagedPlayer, winner);
+        const scoreExpected = deuce();
+        expect(score).toStrictEqual(scoreExpected);
+      })
+    );
+  });
+
+  test('Given a player at 40 when the same player wins, score is Game for this player', () => {
+    fc.assert(
+      fc.property(G.getForty(), G.getPlayer(), (fortyData, winner) => {
+        fc.pre(isSamePlayer(fortyData.player, winner));
+        const score = scoreWhenForty(fortyData, winner);
+        const scoreExpected = game(winner);
+        expect(score).toStrictEqual(scoreExpected);
+      })
+    );
+  });
+
+  test('Given player at 40 and other at 30 when other wins, score is Deuce', () => {
+    fc.assert(
+      fc.property(G.getForty(), G.getPlayer(), (fortyData, winner) => {
+        fc.pre(!isSamePlayer(fortyData.player, winner));
+        fc.pre(fortyData.otherPoint === 30);
+        const score = scoreWhenForty(fortyData, winner);
+        const scoreExpected = deuce();
+        expect(score).toStrictEqual(scoreExpected);
+      })
+    );
+  });
+
+  test('Given player at 40 and other at 15 when other wins, score is 40 - 15', () => {
+    fc.assert(
+      fc.property(G.getForty(), G.getPlayer(), (fortyData, winner) => {
+        fc.pre(!isSamePlayer(fortyData.player, winner));
+        fc.pre(fortyData.otherPoint === 15);
+        const score = scoreWhenForty(fortyData, winner);
+        const scoreExpected = forty(fortyData.player, thirty().value);
+        expect(score).toStrictEqual(scoreExpected);
+      })
+    );
+  });
+
+  test('Given players at 0 or 15 points score kind is still POINTS', () => {
+    fc.assert(
+      fc.property(G.getPoints(), G.getPlayer(), ({ pointsData }, winner) => {
+        const score = scoreWhenPoint(pointsData, winner);
+        expect(score.kind).toStrictEqual('POINTS');
+      })
+    );
+  });
+
+  test('Given one player at 30 and win, score kind is forty', () => {
+    fc.assert(
+      fc.property(G.getPoints(), G.getPlayer(), ({ pointsData }, winner) => {
+        fc.pre(pointsData.PLAYER_ONE === 30 || pointsData.PLAYER_TWO === 30);
+        const score = scoreWhenPoint(pointsData, winner);
+        expect(score.kind).toStrictEqual('FORTY');
+      })
+    );
+  });
 });
